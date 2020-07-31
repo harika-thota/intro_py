0a447fae6072934b92c302115aa91cc1d2f18960
harika-thota <totaharika1@gmail.com>
2020-07-31 11:56:34 -0500

    adding squareroot

---
+++ L7_squr_root.py
@@ -0,0 +1,7 @@
+# floor division 
+"""Floor division - division that results into whole number adjusted to the left in the number line"""
+tent = 2
+vent = 4
+print('tent // vent =',tent // vent)
+#Exponent - left operand raised to the power of right
+print('tent**vent= ', tent**vent) #(x**y = x to the power of y)
