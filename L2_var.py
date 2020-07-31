061718948ab1c9b73896e41ce8367e772b1bc73e
harika-thota <totaharika1@gmail.com>
2020-07-31 11:33:59 -0500

    turtle

---
+++ L2_var.py
@@ -0,0 +1,37 @@
+#variables
+#declaring a value to a variable "credit-card"
+credit_card =44433333333355555555552 
+print(credit_card)   #prints value of credit_card
+
+#sum of variables
+sam = 54
+dom = 76
+print(sam + dom)
+x = sam * dom     #storing two different variable values into another variable 
+print(x)
+#for loop runs 3 times
+for i in range (3) : 
+    x = 32
+    print(x)        #considers x which is inside for loop
+
+
+# basic math
+#division in python
+print(150/2)
+print(8/2)
+print(1236/33)
+print(1236//33) # "//"  rounds up the number
+print(50%5) #reminder
+print(sam +40)
+print(100%3)
+
+#division in turtle 
+import turtle               
+code = turtle.Turtle()           #initiating a turtle 
+x= 8
+for i in range (x//2) : 
+#for loop cannot iterate if there is any decimal number after division. so its better to use '//' 
+ #'//' rounds up the number
+    code.fd(200); code.rt(90);
+
+
