4b5b872aec1f4a122a30b6732639c89739f20bc6
harika-thota <totaharika1@gmail.com>
2020-07-31 11:48:15 -0500

    adding L4

---
+++ L4_data_types.py
@@ -0,0 +1,66 @@
+#data types - int, float, complex
+bananas  = 3
+print('no. of bananas are ', bananas)
+#type() function used to know which class a variable or a value belongs to 
+print('the type of bananas are ',type(bananas))
+apples = 5.9
+print('the type of apples are ',type(apples))
+
+#isinstance() func used to check if an object belongs to a particular class
+d = 1+2j
+print(d, 'is a complex number?',isinstance(1+2j, complex))
+
+
+
+
+
+
+
+
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
